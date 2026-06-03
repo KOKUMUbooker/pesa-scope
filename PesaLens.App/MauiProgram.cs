fede494a347638eeab0b177955904e6dd81f9bc0
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-03 11:19:42 +0300

    Register repositories and database seeder as singleton services

--- PesaLens.App/MauiProgram.cs
+++ PesaLens.App/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using InputKit.Shared.Controls;
 using Mopups.Hosting;
+using PesaLens.App.Data.Repositories.Interfaces;
+using PesaLens.App.Repositories;
 using UraniumUI;
 
 namespace PesaLens.App
@@ -8,6 +10,10 @@ namespace PesaLens.App
     {
         public static MauiApp CreateMauiApp()
         {
+            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "pesalens.db");
+            var dbService = new DatabaseService(dbPath);
+            var connection = await dbService.InitializeAsync();
+
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
@@ -22,9 +28,24 @@ namespace PesaLens.App
 
                     fonts.AddMaterialSymbolsFonts();
                 });
-
             builder.Services.AddMopupsDialogs();
-            return builder.Build();
+            builder.Services.AddSingleton(connection);
+            builder.Services.AddSingleton<ITransactionRepository, TransactionRepository>();
+            builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddSingleton<IAutoCategorizationRuleRepository, AutoCategorizationRuleRepository>();
+            builder.Services.AddSingleton<IBudgetRepository, BudgetRepository>();
+            builder.Services.AddSingleton<IOverallBudgetRepository, OverallBudgetRepository>();
+            builder.Services.AddSingleton<ISyncMetadataRepository, SyncMetadataRepository>();
+            builder.Services.AddSingleton<IAppSettingsRepository, AppSettingsRepository>();
+            builder.Services.AddSingleton<ISecuritySettingsRepository, SecuritySettingsRepository>();
+            builder.Services.AddSingleton<IExportHistoryRepository, ExportHistoryRepository>();
+            builder.Services.AddSingleton<DatabaseSeeder>();
+
+            var app = builder.Build();
+            var seeder = app.Services.GetRequiredService<DatabaseSeeder>();
+            await seeder.SeedAsync();
+
+            return app;
         }
     }
 }

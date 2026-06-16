f46b3bd5377ea682335e966aa963d638ce2e41c6
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-17 01:01:16 +0300

    Add sms stubs required to make app defauls sms handler

---
+++ PesaLens.App/Platforms/Android/HeadlessSmsSendService.cs
@@ -0,0 +1,12 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace PesaLens.App;
+
+[Service(Exported = true, Permission = "android.permission.SEND_RESPOND_VIA_MESSAGE")]
+[IntentFilter(["android.intent.action.RESPOND_VIA_MESSAGE"], Categories = ["android.intent.category.DEFAULT"], DataSchemes = ["sms", "smsto", "mms", "mmsto"])]
+public class HeadlessSmsSendService : Service
+{
+    public override IBinder? OnBind(Intent? intent) => null;
+}

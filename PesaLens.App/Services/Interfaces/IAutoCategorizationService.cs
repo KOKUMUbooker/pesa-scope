a23b337d44cbfaff5027a7175ae7daa5b4666ff0
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-21 17:54:34 +0300

    Define IAutoCategorizationService interface

---
+++ PesaLens.App/Services/Interfaces/IAutoCategorizationService.cs
@@ -0,0 +1,8 @@
+﻿using PesaLens.Core.Models;
+
+namespace PesaLens.App.Services.Interfaces;
+
+public interface IAutoCategorizationService
+{
+    public Task CategorizeAsync(IList<Transaction> transactions);
+}

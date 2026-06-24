a8d33becd1c27fc58755621a976cbd9610a1c7a5
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-24 12:45:04 +0300

    Create custom shell renderer to apply color theme on bottom tab on first render as it used to show white flash

---
+++ PesaLens.App/Platforms/Android/ShellTabBarHandler.cs
@@ -0,0 +1,42 @@
+﻿using Android.Content.Res;
+using Google.Android.Material.BottomNavigation;
+using Microsoft.Maui.Controls.Handlers.Compatibility;
+using Microsoft.Maui.Controls.Platform.Compatibility;
+
+namespace PesaLens.App.Platforms.Android;
+
+public class PesaLensShellHandler : ShellRenderer
+{
+    protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
+    {
+        return new PesaLensBottomNavTracker();
+    }
+}
+
+public class PesaLensBottomNavTracker : IShellBottomNavViewAppearanceTracker
+{
+    public void ResetAppearance(BottomNavigationView bottomView)
+    {
+        ApplyColors(bottomView);
+    }
+
+    public void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
+    {
+        ApplyColors(bottomView);
+    }
+
+    public void Dispose() { }
+
+    private static void ApplyColors(BottomNavigationView bottomView)
+    {
+        // Resolve dark mode
+        var uiMode = bottomView.Context?.Resources?.Configuration?.UiMode & UiMode.NightMask;
+        bool isDark = uiMode == UiMode.NightYes;
+
+        var bgColor = isDark
+            ? global::Android.Graphics.Color.ParseColor("#111F19")
+            : global::Android.Graphics.Color.ParseColor("#F7FAF8");
+
+        bottomView.SetBackgroundColor(bgColor);
+    }
+}

5a7a991ab00620aff2c201b8a1f729e853cb6b66
KOKUMUbooker <bookerochieng20@gmail.com>
2026-06-28 15:35:31 +0300

    Create converter to convert string to hex color

--- PesaLens.App/Converters/CategoriesConverters.cs
+++ PesaLens.App/Converters/CategoriesConverters.cs
@@ -99,3 +99,19 @@ public class NullToStringConverter : IValueConverter
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotImplementedException();
 }
+
+public class StringToColorConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string hex)
+        {
+            try { return Color.FromArgb(hex); }
+            catch { }
+        }
+        return Colors.Gray;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}

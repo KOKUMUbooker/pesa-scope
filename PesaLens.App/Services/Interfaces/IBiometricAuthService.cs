80c97aadd7068572202f6717c202d8c853bf4abc
KOKUMUbooker <bookerochieng20@gmail.com>
2026-07-01 15:55:59 +0300

    Define IBiometricAuthService interface

---
+++ PesaLens.App/Services/Interfaces/IBiometricAuthService.cs
@@ -0,0 +1,16 @@
+﻿namespace PesaLens.App.Services.Interfaces;
+
+public enum BiometricCheckResult
+{
+    Success,
+    Failed,
+    Cancelled,
+    NotAvailable,
+    Error
+}
+
+public interface IBiometricAuthService
+{
+    Task<bool> IsAvailableAsync();
+    Task<BiometricCheckResult> AuthenticateAsync(string reason = "Unlock PesaLens");
+}

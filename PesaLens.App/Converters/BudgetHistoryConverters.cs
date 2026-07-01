666486b723e9fcafee66b75240668c593127b0c7
KOKUMUbooker <bookerochieng20@gmail.com>
2026-07-02 00:31:20 +0300

    Add conveter AllTrueConverter

---
+++ PesaLens.App/Converters/BudgetHistoryConverters.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace PesaLens.App.Converters;
+
+/// <summary>
+/// Returns true only when ALL bound bool values are true.
+/// Use with MultiBinding to gate visibility on multiple conditions.
+/// </summary>
+public class AllTrueConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) =>
+        values.All(v => v is true);
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}

6d8516c0df6d397edb18beb70f6ec73b774b07aa
KOKUMUbooker <bookerochieng20@gmail.com>
2026-09-22 16:59:56 +0300

    Define and implement MpesaSMSSyncService

---
+++ PesaScope.App/Services/Interfaces/IMpesaSMSSyncService.cs
@@ -0,0 +1,8 @@
+﻿namespace PesaScope.App.Services.Interfaces;
+
+public record SyncResult(bool Success, int Inserted, int Duplicates, bool NoPermission, string? Error);
+
+public interface IMpesaSMSSyncService
+{
+    Task<SyncResult> SyncAsync();
+}

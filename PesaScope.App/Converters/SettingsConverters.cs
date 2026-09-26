bb932e2d6842bc41bf96d0b3510a07f37847b9da
KOKUMUbooker <bookerochieng20@gmail.com>
2026-09-26 19:38:34 +0300

    Add converters to be used by rules import and export pages

---
+++ PesaScope.App/Converters/SettingsConverters.cs
@@ -0,0 +1,22 @@
+﻿using PesaScope.Core.Models;
+using System.Globalization;
+
+namespace PesaScope.App.Converters;
+
+public class StatusIsModifiedConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is RuleImportStatus.Modified;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotSupportedException();
+}
+
+public class IntIsGreaterThanZeroConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is int i && i > 0;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotSupportedException();
+}

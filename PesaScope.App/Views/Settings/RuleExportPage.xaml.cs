66e9639b55805b1e171ad0a2a0ffad4ff39a401d
KOKUMUbooker <bookerochieng20@gmail.com>
2026-09-26 19:36:41 +0300

    Add rule export screen for previewing rules that are to be exported

---
+++ PesaScope.App/Views/Settings/RuleExportPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using PesaScope.App.ViewModels;
+
+namespace PesaScope.App.Views.Settings;
+
+public partial class RuleExportPage : UraniumUI.Pages.UraniumContentPage
+{
+    private readonly RuleExportViewModel _vm;
+    private bool _loaded;
+
+    public RuleExportPage(RuleExportViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = _vm = vm;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (_loaded) return;
+        _loaded = true;
+
+        await _vm.LoadAsync();
+    }
+}
